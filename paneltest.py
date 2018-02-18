ae99e331cc3ced8d19f9bf37c3469a8ec418e09d
Jason <jason@Marys-MacBook-Pro.local>
2018-02-18 12:17:36 -0500

    panel object

--- paneltest.py
+++ paneltest.py
@@ -33,65 +33,6 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
-
-
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-	"""Wipe color across display a pixel at a time."""
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, color)
-		strip.show()
-		time.sleep(wait_ms/1000.0)
-
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-	"""Movie theater light style chaser animation."""
-	for j in range(iterations):
-		for q in range(3):
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, color)
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
-
-def wheel(pos):
-	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
-
-def rainbow(strip, wait_ms=20, iterations=1):
-	"""Draw rainbow that fades across all pixels at once."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((i+j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
-
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-	"""Draw rainbow that uniformly distributes itself across all pixels."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
-
-def theaterChaseRainbow(strip, wait_ms=50):
-	"""Rainbow movie theater light style chaser animation."""
-	for j in range(256):
-		for q in range(3):
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, wheel((i+j) % 255))
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
-
 def panelOn(strip, idx, c=Color(255,255,255)):
 
     for j in range(idx * 32, idx * 32 + 32):
@@ -181,34 +122,16 @@ if __name__ == '__main__':
 	# Intialize the library (must be called once before other functions).
 	strip.begin()
 
+        p = Panel(strip, 3, 32)
+
+
 	print ('Press Ctrl-C to quit.')
 	while True:
-#		print ('Color wipe animations.')
-#		colorWipe(strip, Color(255, 0, 0), 5)  # Red wipe
-#		colorWipe(strip, Color(0, 255, 0), 5)  # Blue wipe
-#		colorWipe(strip, Color(0, 0, 255), 5)  # Green wipe
-#		print ('Theater chase animations.')
-#		theaterChase(strip, Color(127, 127, 127), 5)  # White theater chase
-#		theaterChase(strip, Color(127,   0,   0), 5)  # Red theater chase
-#		theaterChase(strip, Color(  0,   0, 127), 5)  # Blue theater chase
-#		print ('Rainbow animations.')
-#		rainbow(strip)
-#		rainbowCycle(strip)
-#		theaterChaseRainbow(strip)
-#                print ('Blinking 10')
-#                blink(strip, 10)
-#                print ('Gracie\'s function!')
-#                gfunc(strip, 5000)
-#                print ('Blinking 50')
-#                blink(strip, 50)
-#                print ('Blinking 100')
-#                blink(strip, 100)
-#                print ('Left/Right 50')
-#                leftright(strip, 50)
-#                print ('Left/Right 200')
-#                leftright(strip, 200)
-#                print ('Blinking 250')
-#                blink(strip, 250)
-#                print('Blinking 500')
-#                blink(strip, 500)
-                blinkPanels(strip,  250)
+                p.setColor(Color(255,255,0))
+                strip.show()
+
+                time.sleep(0.5)
+
+                p.setColor(Color(0, 0, 0))
+                strip.show()
+                time.sleep(0.5)
