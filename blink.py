84e0085b21743cea287d5c9260e6e0c507ba7e81
jmodlin <jason@modlin.name>
2018-02-22 13:59:16 -0500

    Update blink.py

---
+++ blink.py
@@ -0,0 +1,29 @@
+# Stock libraries
+import time
+import random
+import signal
+import sys
+
+# Custom objects
+from panel import *
+from animation import *
+from display import *
+
+class Blink(Animation):
+
+    def __init__(self, display):        
+        self.display = display
+    
+    def begin(self):
+        self.display.clear()
+        for i in range(15):
+            for p in self.display.panels:
+                p.setPanelColor(Color(255,0,0))
+                self.display.update()
+                time.sleep(2.0)
+            self.display.clear()
+            time.sleep(2.0)
+
+    def end(self):
+        self.display.clear()
+        
