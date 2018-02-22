55d38f84abe73f1e3c64cd4423cdbc9e4e456fd2
Modlin <CAR0182@nac.ppg.com>
2018-02-22 13:51:57 -0500

    m

--- display.py
+++ display.py
@@ -8,21 +8,9 @@ import argparse
 import signal
 import sys
 
-def signal_handler(signal, frame):
-        colorWipe(strip, Color(0,0,0))
-        sys.exit(0)
-
-def opt_parse():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-c', action='store_true', help='clear the display on exit')
-        args = parser.parse_args()
-        if args.c:
-                signal.signal(signal.SIGINT, signal_handler)
-
 # LED strip configuration:
 LED_COUNT      = 160      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
@@ -32,9 +20,16 @@ LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 class Display:
 
-    def __init__(self, strip, pCount, ppp):
-        self.strip = strip
+    def __init__(self, pCount, ppp):
         
+        # Ensure LED_COUNT is correct
+        LED_COUNT = (pCount * ppp)
+
+        # Initialize LED strip
+        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+        self.strip.begin()
+
+        # Initialize panel objects
         self.panels = []
         for i in range(pCount):
             panels.append(Panel(strip, i, ppp))
@@ -49,24 +44,3 @@ class Display:
         for i in range(self.numPanels):
             panel.setPanelColor(Color(0, 0, 0))
         self.strip.show()
-        
-
-        
-
-
-# Main program logic follows:
-if __name__ == '__main__':
-    # Process arguments
-    opt_parse()
-
-	# Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-	# Intialize the library (must be called once before other functions).
-    strip.begin()
-
-    # Create display with 5 panels, 32 pixels per panel
-    display = Display(strip, 5, 32)
-        
-	print ('Press Ctrl-C to quit.')
-	while True:
-                
