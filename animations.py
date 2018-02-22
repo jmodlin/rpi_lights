148888d306e4d0a029fa533fed7f2a5c014b5916
Jason <jason@Marys-MacBook-Pro.local>
2018-02-22 18:41:35 -0500

    a

--- animations.py
+++ animations.py
@@ -51,7 +51,7 @@ class Kitt(Animation):
 
                 # 200 / 1000 * (1 - (0/5)) =200  
                 delay = (self.ms/1000.0) * (1.0 - (p/self.display.numPanels))
-                print('p->' + p + ' .. delay->' + delay)
+                print('p->' + `p` + ' .. delay->' + `delay`)
                 time.sleep(delay)
             time.sleep(self.ms/4/1000.0)
             # Going down the panels
@@ -61,7 +61,7 @@ class Kitt(Animation):
                 self.display.panels[p].setPanelColor(self.color)
                 self.display.update()
                 delay = (self.ms/1000.0) * (1.0 - (self.display.numPanels-p+1/self.display.numPanels))
-                print('p->' + p + ' .. delay->' + delay)
+                print('p->' + `p` + ' .. delay->' + `delay`)
                 time.sleep(delay)
             time.sleep(self.ms/4/1000.0)
 
