3cf435ac6dfdbfae0b15865ca2525c2e6c28b3e0
Jason <jason@Marys-MacBook-Pro.local>
2018-02-22 23:35:09 -0500

    a

--- show.py
+++ show.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     colors = [Color(0, 255, 10), Color(255, 215, 0)]
     shimmer = Shimmer(display, colors, 150, 600)
 
-    rocker = Rocker(display, Color(0,255,10), Color(255,215,0), 20, 1)
+    rocker = Rocker(display, Color(0,255,10), Color(255,215,0), 20, 20)
     #animations = [redblinky, greenblinky, kitt]
     animations = [rocker]
 
