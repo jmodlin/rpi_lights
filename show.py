1eacc463a19e3aa591419416c40409fab1dc8e34
Jason <jason@Marys-MacBook-Pro.local>
2018-02-22 22:00:15 -0500

    a

--- show.py
+++ show.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     greenblinky = Blink(display, Color(0,255,0), 10, 250)
     kitt = Kitt(display, Color(255,0,0), 20, 300)
 
-    colors = [Color(212, 36, 38), Color(36, 214, 211), Color(39, 214, 36), Color(211, 36, 214), Color(122, 36, 214)]
+    colors = [Color(212, 36, 38), Color(36, 214, 211), Color(39, 214, 36), Color(211, 36, 214), Color(122, 36, 214), Color(255, 255, 255), Color(255, 255, 255)]
     shimmer = Shimmer(display, colors, 15, 2000)
 
     #animations = [redblinky, greenblinky, kitt]
