1e5a96541be745b5da095bb25813fbd102f97ac0
Jason <jason@Marys-MacBook-Pro.local>
2018-02-22 22:32:40 -0500

    a

--- show.py
+++ show.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
     #colors = [Color(212, 36, 38), Color(36, 214, 211), Color(39, 214, 36), Color(211, 36, 214), Color(122, 36, 214), Color(255, 255, 255), Color(255, 255, 255)]
     colors = [Color(212, 36, 38), Color(36, 214, 211), Color(39, 214, 36)]
-    shimmer = Shimmer(display, colors, 150, 400)
+    shimmer = Shimmer(display, colors, 150, 600)
 
     #animations = [redblinky, greenblinky, kitt]
     animations = [shimmer]
