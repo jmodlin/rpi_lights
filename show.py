0334a80ed2d0c848eb3c49a180e9c7ea1d3d0a33
Jason <jason@Marys-MacBook-Pro.local>
2018-02-24 11:58:23 -0500

    a

--- show.py
+++ show.py
@@ -45,6 +45,7 @@ if __name__ == '__main__':
     xmasColors = [Color(255, 0, 0), Color(0, 255, 0), Color(212, 36, 38), Color(36, 214, 211), Color(39, 214, 36), Color(211, 36, 214), Color(122, 36, 214)]
 
     # Halloween colors
+    halloweenColors = [Color(247, 95, 28), Color(136, 30, 228), Color(133, 226, 31)]
 
     # St. Patrick's Day colors    
     stPatColors = [Color(0,255,10), Color(255,215,0), Color(255,215,0), Color(0,255,10)]
@@ -59,9 +60,13 @@ if __name__ == '__main__':
     xmasbreath = Breathing(display, xmasColors[:2], 1000, 20)
     xmasrocker = Rotator(display, xmasColors[0], xmasColors[1], False, 5, 100)
     xmasshimmer2 = Shimmer(display, xmasColors[:2], 50, 600)
-    
-    
-    animations = [rocker, shimmer1, breath, shimmer2, xmasrocker, xmasshimmer1, xmasbreath, xmasshimmer2]
+
+    halloshimmer1 = Shimmer(display, halloweenColors, 50, 600)
+    hallobreath = Breathing(display, halloweenColors[:2], 1000, 20)
+    hallorocker = Rotator(display, halloweenColors[0], halloweenColors[1], False, 5, 100)
+    halloshimmer2 = Shimmer(display, halloweenColors[:2], 50, 600)
+
+    animations = [rocker, shimmer1, breath, shimmer2, xmasrocker, xmasshimmer1, xmasbreath, xmasshimmer2, hallorocker, halloshimmer1, hallobreath, halloshimmer2]
 
     # Initialize show
     show = Show(display, animations)
