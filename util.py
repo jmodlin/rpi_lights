e0efd3fb425ba51199e31aaf31a597bc58db61f9
Jason <jason@Marys-MacBook-Pro.local>
2018-02-25 21:32:48 -0500

    a

--- util.py
+++ util.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     c = rgb2color(rgb)
     print c
  
-    colors = color_run(Color(255,0,0), Color(0,212,255), 20)
+    colors = color_run(Color(255,0,0), Color(0,212,255), 200)
     print 'color_run ->'
     print colors
  
