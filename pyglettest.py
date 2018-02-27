727ff2bc42a9704b4dfde07c985ba5aeb33bb5ec
Jason <jason@Marys-MacBook-Pro.local>
2018-02-26 20:12:05 -0500

    a

--- pyglettest.py
+++ pyglettest.py
@@ -2,6 +2,6 @@ import pyglet
 
 pyglet.options['audio'] = ('openal', 'pulse', 'silent')
 
-march = pyglet.media.load('Game_Room.mp3')
+march = pyglet.media.load('/home/pi/Documents/rpi_porch/rpi_lights/Game_Room.mp3')
 march.play()
 
