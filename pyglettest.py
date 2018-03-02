49471d1dc560a199109e896754dab0d45feba3f5
Jason <jason@Marys-MacBook-Pro.local>
2018-03-01 20:58:09 -0500

    a

--- pyglettest.py
+++ pyglettest.py
@@ -14,7 +14,7 @@ if 1:
     import pygame.display
     os.putenv('SDL_VIDEODRIVER', 'fbcon')
     pygame.display.init()
-    screen = pygame.display.set_mode((1,1))
+    #screen = pygame.display.set_mode((1,1))
     pygame.time.set_timer(USEREVENT+1, 5000)
 
 def quit():
