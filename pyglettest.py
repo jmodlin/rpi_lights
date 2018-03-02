aba16e67de8b9f6ac8b3a3f340c386d0aa401805
Jason <jason@Marys-MacBook-Pro.local>
2018-03-01 20:23:44 -0500

    a

--- pyglettest.py
+++ pyglettest.py
@@ -6,6 +6,7 @@ from pygame.locals import *
 
 
 def quit():
+    print 'quiting !!'
     pygame.quit()
 
 def main():
@@ -31,15 +32,23 @@ pygame.display.init()
 screen = pygame.display.set_mode((1,1))
 
 print ('loading music file ...')
-pygame.mixer.init()
-pygame.mixer.music.load('/media/share/Game_Room.mp3')
-pygame.mixer.music.set_volume(0.8)
-pygame.mixer.music.play()
+#pygame.mixer.init()
+#pygame.mixer.music.load('/media/share/Game_Room.mp3')
+#pygame.mixer.music.set_volume(0.8)
+#pygame.mixer.music.play()
+pygame.time.set_timer(USEREVENT+1, 5000)
 
-while pygame.mixer.music.get_busy(): 
-    pygame.time.Clock().tick(10)
+while True:
     for event in pygame.event.get():
-        print 'event -> ' + `event`
+            if event.type == USEREVENT+1:
+                quit()
+
+#while pygame.mixer.music.get_busy(): 
+#    pygame.time.Clock().tick(10)
+    
+
+#    for event in pygame.event.get():
+#        print 'event -> ' + `event`
 
 
 print ('done')
