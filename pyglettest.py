e5e1efd51866116660f5fc222154ee1543a5d2e4
Jason <jason@Marys-MacBook-Pro.local>
2018-03-01 21:01:10 -0500

    a

--- pyglettest.py
+++ pyglettest.py
@@ -1,18 +1,15 @@
 import os, sys
 from pygame.locals import *
-
+import pygame.display
 # set SDL to use the dummy NULL video driver, 
 #   so it doesn't need a windowing system.
-os.environ["SDL_VIDEODRIVER"] = "dummy"
-
-
-import pygame.transform
-
+os.environ["SDL_VIDEODRIVER"] = "directfb"
+os.putenv('SDL_VIDEODRIVER', 'fbcon')
 
 if 1:
     #some platforms might need to init the display for some parts of pygame.
-    import pygame.display
-    os.putenv('SDL_VIDEODRIVER', 'fbcon')
+
+    
     pygame.display.init()
     #screen = pygame.display.set_mode((1,1))
     pygame.time.set_timer(USEREVENT+1, 5000)
