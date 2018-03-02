f304ac4d0793fddb01412a05bcfbcbdc99b3f499
Jason <jason@Marys-MacBook-Pro.local>
2018-03-01 20:55:40 -0500

    a

--- pyglettest.py
+++ pyglettest.py
@@ -1,53 +1,29 @@
+import os, sys
 
-import os
-import sys
-import pygame
-from pygame.locals import *
+# set SDL to use the dummy NULL video driver, 
+#   so it doesn't need a windowing system.
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+
+import pygame.transform
+
+
+if 1:
+    #some platforms might need to init the display for some parts of pygame.
+    import pygame.display
+    os.putenv('SDL_VIDEODRIVER', 'fbcon')
+    pygame.display.init()
+    screen = pygame.display.set_mode((1,1))
+
 
 
 def quit():
-    print 'quiting !!'
     pygame.quit()
 
-def main():
-    print 'Starting game loop ...'
-    while True:
+if __name__ == "__main__":
     
-        print pygame.time.get_ticks()
-
-        for ev in pygame.event.get():
-            print ('ev ->' + `ev`)
-            if ev.type == KEYDOWN:
-                    print 'keydown -> ' + ev.key
-                    if ev.key == K_q:
-                        quit()
-        
-    pygame.quit()
-
-os.environ["SDL_VIDEODRIVER"] = "dummy" # Removes ...
-
-pygame.init()
-pygame.display.init()
-screen = pygame.display.set_mode((1,1))
-
-print ('loading music file ...')
-#pygame.mixer.init()
-#pygame.mixer.music.load('/media/share/Game_Room.mp3')
-#pygame.mixer.music.set_volume(0.8)
-#pygame.mixer.music.play()
-pygame.time.set_timer(USEREVENT+1, 5000)
-
-while True:
-    for event in pygame.event.get():
+    while True:
+        for event in pygame.event.get():
             if event.type == USEREVENT+1:
                 quit()
 
-#while pygame.mixer.music.get_busy(): 
-#    pygame.time.Clock().tick(10)
-    
-
-#    for event in pygame.event.get():
-#        print 'event -> ' + `event`
-
-
-print ('done')
