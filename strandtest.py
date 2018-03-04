4f20f68f07d857ec8891092316ae1938e2b704fb
Jason <jason@Marys-MacBook-Pro.local>
2018-03-04 12:01:43 -0500

    a

--- strandtest.py
+++ strandtest.py
@@ -64,11 +64,16 @@ def wheel(pos):
 		pos -= 170
 		return Color(0, pos * 3, 255 - pos * 3)
 
+def writeColor(c):
+	print hex(c)
+
 def rainbow(strip, wait_ms=20, iterations=1):
 	"""Draw rainbow that fades across all pixels at once."""
 	for j in range(256*iterations):
 		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((i+j) & 255))
+			c = wheel((i+j) & 255)
+			strip.setPixelColor(i, c)
+			writeColor(c)
 		strip.show()
 		time.sleep(wait_ms/1000.0)
 
@@ -100,6 +105,10 @@ if __name__ == '__main__':
 	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 	# Intialize the library (must be called once before other functions).
 	strip.begin()
+
+
+	rainbow(strip)
+	exit()
 
 	print ('Press Ctrl-C to quit.')
 	while True:
